f246f2e100c916853bec1cc907e709a65615f432
Kacper <k.kustra100@gmail.com>
2022-12-04 21:07:30 +0100

    Add password hashing to User model

--- src/config/variables.ts
+++ src/config/variables.ts
@@ -1,14 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-interface IVariables {
+export interface IVariables {
   port: number;
   mongoUrl: string;
+  saltWorkFactor: number;
 }
 
 const variables: IVariables = {
   port: Number(process.env.PORT) || 5000,
   mongoUrl: process.env.MONGO_URL || '',
+  saltWorkFactor: 10,
 };
 
 export default variables;
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,27 +1,27 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model, InferSchemaType, Model } from 'mongoose';
+import validator from 'validator';
+import bcrypt from 'bcrypt';
+import configVariables from '../config/variables';
 
-interface IUser {
-  name: string;
-  email: string;
-  password: string;
-  createdAt: Date;
-  updatedAt: Date;
-}
-
-const userSchema = new Schema<IUser>(
+const userSchema = new Schema(
   {
     name: {
       type: String,
-      required: [true, 'Name must be provided'],
+      required: [true, 'Name must be provided.'],
     },
     email: {
       type: String,
-      required: [true, 'Email must be provided'],
+      required: [true, 'Email must be provided.'],
       unique: true,
+      validate: {
+        validator: validator.isEmail as (str: string) => boolean,
+        message: 'Please provide valid email',
+      },
     },
     password: {
       type: String,
-      required: [true, 'Password must be provided'],
+      required: [true, 'Password must be provided.'],
+      minLength: [8, 'Password must be at least 8 characters long.'],
     },
   },
   {
@@ -29,4 +29,24 @@ const userSchema = new Schema<IUser>(
   }
 );
 
-export default model<IUser>('User', userSchema);
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) next();
+
+  const salt = await bcrypt.genSalt(configVariables.saltWorkFactor);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  return next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword: string): Promise<boolean> {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+interface IUserMethods {
+  comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+export type User = InferSchemaType<typeof userSchema>;
+export type UserModel = Model<User, {}, IUserMethods>;
+
+export default model<User, UserModel>('User', userSchema);
