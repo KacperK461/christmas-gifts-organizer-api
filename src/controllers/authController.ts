0298db268d238e6e6fd73bac005b7c7c022926e1
Kacper <k.kustra100@gmail.com>
2022-12-16 18:28:38 +0100

    Create register endpoint

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,10 +1,23 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import { RegisterUserInput } from '../schemas/user.schema';
+import User from '../models/User';
+import { StatusCodes } from 'http-status-codes';
+
+import { BadRequestError } from '../utils/errors';
+import { attachTokenCookies, createRefreshToken } from '../utils/auth';
 
 export const register = async (req: Request<{}, {}, RegisterUserInput['body']>, res: Response) => {
-  const { email, name, password } = req.body;
+  const { name, email, password } = req.body;
 
-  res.send({ email, name, password });
+  const isDuplicateEmail = await User.findOne({ email });
+  if (isDuplicateEmail) throw new BadRequestError('This email address is already taken.');
+
+  const user = await User.create({ name, email, password });
+
+  const refreshToken = await createRefreshToken(req, user.id);
+  await attachTokenCookies(res, user.id, refreshToken);
+
+  return res.status(StatusCodes.CREATED).send(user);
 };
 
 export const login = async (req: Request, res: Response) => {};
